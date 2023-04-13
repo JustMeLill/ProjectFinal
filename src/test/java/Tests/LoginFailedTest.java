3be7f20206511d06ee5ecfd9c73cf97346c824ce
JustMeLill <dutescu_lilly@yahoo.com>
2023-04-13 14:16:36 +0300

    Added Shared Data

--- src/test/java/Tests/LoginFailedTest.java
+++ src/test/java/Tests/LoginFailedTest.java
@@ -2,14 +2,9 @@ package Tests;
 
 import SharedData.SharedData;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginFailedTest extends SharedData {
 
--- src/test/java/Tests/LoginSuccessfulTest.java
+++ src/test/java/Tests/LoginSuccessfulTest.java
@@ -2,13 +2,8 @@ package Tests;
 
 import SharedData.SharedData;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginSuccessfulTest extends SharedData {
     @Test
